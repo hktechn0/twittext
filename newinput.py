3464fd023396eddfcaed8985e7fd656b1349bb28
Hirotaka Kawata <hktechno@hotmail.com>
2009-11-15 01:52:55 +0900

    modified input method

--- newinput.py
+++ newinput.py
@@ -6,6 +6,7 @@ import curses
 def mbgetstr(stdcur, sety, setx, debug = False):
     s = u""
     i = 0
+    cc = 0
     
     curses.noecho()
 
@@ -35,42 +36,67 @@ def mbgetstr(stdcur, sety, setx, debug = False):
             if x <= setx:
                 continue
 
-            if isascii(s[i - 1], True):
+            if isascii(s[i - 1]):
                 x -= 1
+                cc -= 1
             else:
                 x -= 2
+                cc -= 2
 
             s = s[:i - 1] + s[i:]
             i -= 1
             
-            stdcur.move(y, setx)
+            stdcur.move(sety, setx)
             stdcur.clrtoeol()
-            stdcur.addstr(s.encode("utf-8"))
+
+            if cc + setx > maxx - 1:
+                j = getoffset(s, cc, setx, maxx)
+            else:
+                j = 0
+            
+            stdcur.addstr(s[j:i].encode('utf-8'))
+            (y, x) = stdcur.getyx()
+            stdcur.addstr(s[i:].encode('utf-8'))
             stdcur.move(y, x)
         elif c == curses.KEY_DC:
             if i >= len(s):
                 continue
 
+            if isascii(s[i]):
+                cc -= 1
+            else:
+                cc -= 2
+
             s = s[:i] + s[i + 1:]
 
-            stdcur.move(y, setx)
+            stdcur.move(sety, setx)
             stdcur.clrtoeol()
-            stdcur.addstr(s.encode("utf-8"))
+            
+            if cc + setx > maxx - 1:
+                j = getoffset(s, cc, setx, maxx)
+            else:
+                j = 0
+
+            stdcur.addstr(s[j:i].encode('utf-8'))
+            (y, x) = stdcur.getyx()
+            stdcur.addstr(s[i:].encode('utf-8'))
             stdcur.move(y, x)
         elif c == curses.KEY_LEFT:
             if x <= setx:
                 continue
-            elif isascii(s[i - 1], True):
+            
+            if isascii(s[i - 1]):
                 x -= 1
             else:
                 x -= 2
-
+            
             i -= 1
             stdcur.move(y, x)
         elif c == curses.KEY_RIGHT:
             if i >= len(s):
                 continue
-            if isascii(s[i], True):
+            
+            if isascii(s[i]):
                 x += 1
             else:
                 x += 2
@@ -81,22 +107,40 @@ def mbgetstr(stdcur, sety, setx, debug = False):
             pass
         else:
             if c & 0x80:
-                cc = c << 1
-                while cc & 0x80:
-                    cc <<= 1
+                f = c << 1
+                while f & 0x80:
+                    f <<= 1
                     c <<= 8
                     c += (stdcur.getch() & 0xff)
-
+            
             c = utf2ucs(c)
+            
+            if isascii(c) and not(isprintable(c)):
+                continue
+            
             s = s[:i] + c + s[i:]
-            stdcur.insstr(c.encode("utf-8"))
             i += 1
             
-            if isascii(c, True):
-                stdcur.move(y, x + 1)
+            if isascii(c):
+                cursor = 1
+                cc += 1
             else:
-                stdcur.move(y, x + 2)
-
+                cursor = 2
+                cc += 2
+            
+            if cc + setx > maxx - 1:
+                stdcur.move(sety, setx)
+                stdcur.clrtoeol()
+                j = getoffset(s, cc, setx, maxx)
+                
+                stdcur.addstr(s[j:i].encode('utf-8'))
+                (y, x) = stdcur.getyx()
+                stdcur.addstr(s[i:].encode('utf-8'))
+                stdcur.move(y, x)
+            else:
+                stdcur.insstr(c.encode('utf-8'))
+                stdcur.move(y, x + cursor)
+    
     # for iTerm fix
     try:
         curses.curs_set(0)
@@ -121,20 +165,37 @@ def utf2ucs(utf):
     else:
         # ascii
         ucs = utf
-
+    
     return unichr(ucs)
 
-def isascii(c, printable = False):
+def isascii(c):
     if 0x00 <= ord(c) <= 0x7f:
-        if printable:
-            if 0x20 <= ord(c) <= 0x7e:
-                return True
-            else:
-                return False
-        else:
-            return True
+        return True
     else:
         return False
+
+def isprintable(c):
+    if 0x20 <= ord(c) <= 0x7e:
+        return True
+    else:
+        return False
+
+def getoffset(s, cc, setx, maxx):
+    w = 0
+    j = 0
+
+    for ic in s:
+        j += 1
+        
+        if isascii(ic):
+            w += 1
+        else:
+            w += 2
+            
+        if w > cc + setx - maxx:
+            break
+
+    return j
 
 def _test(stdcur):
     curses.use_default_colors()
