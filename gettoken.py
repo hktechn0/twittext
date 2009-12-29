bd87fab6c25f805e948e0a4419798f9664613ef6
Hirotaka Kawata <hktechno@hotmail.com>
2009-12-30 04:20:48 +0900

    Release: v0.5

--- gettoken.py
+++ gettoken.py
@@ -6,6 +6,23 @@
 # - Hirotaka Kawata <info@techno-st.net>
 # - http://www.techno-st.net/wiki/Twittext
 #
+#    Copyright (C) 2009 Hirotaka Kawata <info@techno-st.net>
+#
+#    This file is part of "Twittext".
+#
+#    Twittext is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Twittext is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with Twittext.  If not, see <http://www.gnu.org/licenses/>.
+#
 
 import twoauth
 
